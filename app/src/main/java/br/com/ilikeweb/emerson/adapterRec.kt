59f799af5f4e9208b296560634eaf1cfa8a51458
emersonrodrigues <emersonrf227@yahoo.com.br>
2019-04-22 19:07:33 -0300

    salvando corretamente porem com erro no ToLong

--- app/src/main/java/br/com/ilikeweb/emerson/adapterRec.kt
+++ app/src/main/java/br/com/ilikeweb/emerson/adapterRec.kt
@@ -1,7 +1,14 @@
 package br.com.ilikeweb.emerson
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import br.com.ilikeweb.emerson.model.Receitas
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ValueEventListener
@@ -9,22 +16,53 @@ import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_adapter_rec.*
 
 
-class adapterRec : AppCompatActivity() {
 
+class adapterRec(private val dataList: ArrayList<Receitas>,private val context: Context) :
+    RecyclerView.Adapter<adapterRec.AtividadeViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AtividadeViewHolder {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adapter_rec)
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_lancamento, parent, false)
+        return AtividadeViewHolder(view)
+    }
 
-
-          
-
+    override fun onBindViewHolder(holder: AtividadeViewHolder, position: Int) {
+        holder.lbDesc!!.text = dataList[position].descricao!!
+        holder.lbValor!!.text = dataList[position].valor!!
 
 
     }
 
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+    class AtividadeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var txtLbdesc: TextView
+        var txtLbValor: TextView
+
+        init {
+            txtLbdesc = itemView.findViewById(R.id.lbDesc)
+            txtLbValor = itemView.findViewById(R.id.lbValor)
+        }
 
 
-
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
